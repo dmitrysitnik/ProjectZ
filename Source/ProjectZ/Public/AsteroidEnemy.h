5b402f349beffbdab4d1460619234acd35da54b1
dmitry.sitnik <dmitry.sitnik46@gmail.com>
2019-08-21 07:59:57 +0300

    New logic

---
+++ Source/ProjectZ/Public/AsteroidEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "AsteroidEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZ_API AAsteroidEnemy : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+};
