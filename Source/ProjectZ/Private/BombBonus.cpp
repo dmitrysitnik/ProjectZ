9afe2829471e7a65c93b5652676be490dc84bb99
dmitrysitnik <dmitry.sitnik46@gmail.com>
2019-09-10 00:01:14 +0300

    Comment the new class logic

--- Source/ProjectZ/Private/BombBonus.cpp
+++ Source/ProjectZ/Private/BombBonus.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BombBonus.h"
-#include "Components/SphereComponent.h"
-
-
-void ABombBonus::MakeExplosion(){
-    
-}
+//#include "BombBonus.h"
+//#include "Components/SphereComponent.h"
+//
+//
+//void ABombBonus::MakeExplosion(){
+//
+//}
 
