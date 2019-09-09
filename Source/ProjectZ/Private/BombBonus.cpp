dc72f456242b3164cf06d31e89a256542cba1cd3
dmitrysitnik <dmitry.sitnik46@gmail.com>
2019-09-09 23:42:42 +0300

    Add new bonus class

---
+++ Source/ProjectZ/Private/BombBonus.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BombBonus.h"
+#include "Components/SphereComponent.h"
+
+
+void ABombBonus::MakeExplosion(){
+    
+}
+
---
+++ Source/ProjectZ/Public/BombBonus.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BonusBase.h"
+#include "Components/SphereComponent.h"
+#include "BombBonus.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZ_API ABombBonus : public ABonusBase
+{
+	GENERATED_BODY()
+    
+    ABombBonus() : ABonusBase(){
+        ExplosionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere0"));
+        RootComponent = ExplosionSphere;
+    }
+    
+    
+    /** Restrict of the explosion area */
+    USphereComponent* ExplosionSphere;
+    
+public:
+    
+    /** Make an explosion. Destroy all enemies in the explosion area*/
+    void MakeExplosion();
+	
+};
