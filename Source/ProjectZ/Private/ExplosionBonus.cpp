a4fd38c1a20f0359fc9fec154272c0719770147c
dmitrysitnik <dmitry.sitnik46@gmail.com>
2019-09-10 19:44:02 +0300

    Add template functions in the explosion bonus

--- Source/ProjectZ/Private/ExplosionBonus.cpp
+++ Source/ProjectZ/Private/ExplosionBonus.cpp
@@ -11,3 +11,16 @@ void AExplosionBonus::MakeExplosion(){
     
 }
 
+
+
+
+void AExplosionBonus::ApplyBonus(){
+    
+}
+
+
+void AExplosionBonus::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult){
+    
+    
+}
+
--- Source/ProjectZ/Public/ExplosionBonus.h
+++ Source/ProjectZ/Public/ExplosionBonus.h
@@ -16,8 +16,23 @@ class PROJECTZ_API AExplosionBonus : public ABonusBase
     
     
 public:
+    /** Constructor*/
+    AExplosionBonus() : ABonusBase(){
+        //Add function to process overlapping
+        SmBonus->OnComponentBeginOverlap.AddDynamic(this, &AExplosionBonus::OnBeginOverlap);
+    };
+    
     
     /** Make an explosion in the sphere area. Destroy all enemies actors in the area*/
     void MakeExplosion();
 	
+    
+    /** Apply the bonus*/
+    virtual void ApplyBonus() override;
+    
+    
+    UFUNCTION()
+    void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+    
+    
 };
