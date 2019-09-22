9ea11047ce2a8f245da6fae045f97d8320171904
dmitrysitnik <dmitry.sitnik46@gmail.com>
2019-09-22 10:33:09 +0300

    Add WavesController class

---
+++ Source/ProjectZ/Public/WavesController.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECTZ_API WavesController
+{
+public:
+	WavesController();
+	~WavesController();
+};
