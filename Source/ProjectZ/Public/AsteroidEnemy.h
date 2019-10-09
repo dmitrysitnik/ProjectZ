e60bd4ef6e851a6919c53b45de36fa7ad278bb38
dmitrysitnik <dmitry.sitnik46@gmail.com>
2019-10-09 20:36:38 +0300

    Return to the work

--- Source/ProjectZ/Public/AsteroidEnemy.h
+++ Source/ProjectZ/Public/AsteroidEnemy.h
@@ -29,4 +29,5 @@ public:
         UFUNCTION()
         void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
     
+    
 };
