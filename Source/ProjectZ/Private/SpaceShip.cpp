e2a04730a207e9abcf7c0849948bca5c5c4382d3
dmitrysitnik <dmitry.sitnik46@gmail.com>
2020-05-03 16:19:56 +0300

    Change static mesh for player

--- Source/ProjectZ/Private/SpaceShip.cpp
+++ Source/ProjectZ/Private/SpaceShip.cpp
@@ -37,8 +37,10 @@ ASpaceShip::ASpaceShip()
 {
  //	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/TwinStick/Meshes/TwinStickUFO.TwinStickUFO"));
+    
+    //Setting a static mesh for a player ship
+//	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/TwinStick/Meshes/TwinStickUFO.TwinStickUFO"));
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/Models/PlayerShip/SM_SpaceShip.SM_SpaceShip"));
 
 	// Create the mesh component
 	ShipMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShipMesh"));
