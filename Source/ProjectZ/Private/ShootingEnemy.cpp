d9c477de8b40af458b7b6d52eec9be8f0ca41379
dmitrysitnik <dmitry.sitnik46@gmail.com>
2020-05-10 15:48:39 +0300

    Add shooting behavior in Shooting enemy

--- Source/ProjectZ/Private/ShootingEnemy.cpp
+++ Source/ProjectZ/Private/ShootingEnemy.cpp
@@ -7,7 +7,7 @@
 #include "UObject/ConstructorHelpers.h"
 #include "SpaceShip.h"
 #include "Engine/StaticMesh.h"
-
+#include "ProjectZProjectile.h"
 #include "Runtime/Engine/Classes/Particles/ParticleSystemComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/SceneComponent.h"
@@ -35,10 +35,15 @@
 
 AShootingEnemy::AShootingEnemy(){
     PrimaryActorTick.bCanEverTick = true;
-        static ConstructorHelpers::FObjectFinder<UStaticMesh> Mesh(TEXT("/Game/Geometry/Meshes/SM_Rock.SM_Rock"));
-        smEnemy = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShootingMesh"));
-        RootComponent = smEnemy;
-        smEnemy->SetStaticMesh(Mesh.Object);
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> Mesh(TEXT("/Game/Geometry/Meshes/SM_Rock.SM_Rock"));
+    smEnemy = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShootingMesh"));
+    RootComponent = smEnemy;
+    smEnemy->SetStaticMesh(Mesh.Object);
+    
+    
+    GunOffset = FVector(-20.0f, 0.0f, 0.0f);
+    FireRate = 2.0f;
+    
 }
 
 
@@ -46,5 +51,48 @@ void AShootingEnemy::Tick(float deltaSeconds){
     Super::Tick(deltaSeconds);
     
     
+    FireShot();
+}
+
+void AShootingEnemy::BeginPlay(){
+    Super::BeginPlay();
+    bCanFire = true;
     
+}
+
+
+
+void AShootingEnemy::FireShot(){
+    // If it's ok to fire again
+    if (bCanFire == true)
+    {
+
+        const FRotator FireRotation = -1 * GetActorRotation();
+        //Spawn projectile at an offset from this pawn
+        const FVector SpawnLocation = GetActorLocation() + GunOffset;
+
+        UWorld* const World = GetWorld();
+        if (World && ProjectileToSpawn)
+        {
+            //spawn the projectile
+            World->SpawnActor<AProjectZProjectile>(ProjectileToSpawn, SpawnLocation, FireRotation);
+
+
+            bCanFire = false;
+            World->GetTimerManager().SetTimer(TimerHandle_FireTimerExpired, this, &AShootingEnemy::FireTimerExpired, FireRate);
+
+            //try and play the sound if specified
+            //               if (FireSound)
+            //               {
+            //                   UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+            //               }
+
+            bCanFire = false;
+        }
+    }
+}
+
+
+void AShootingEnemy::FireTimerExpired(){
+    bCanFire = true;
 }
--- Source/ProjectZ/Public/ShootingEnemy.h
+++ Source/ProjectZ/Public/ShootingEnemy.h
@@ -15,10 +15,32 @@ class PROJECTZ_API AShootingEnemy : public AEnemyBase
 	GENERATED_BODY()
     
     public:
+    
     AShootingEnemy();
     
+    UPROPERTY(Category = Shooting, EditAnywhere, BlueprintReadWrite)
+    TSubclassOf<class AProjectZProjectile> ProjectileToSpawn;
+    
+    UPROPERTY(Category = Shooting, EditAnywhere, BlueprintReadWrite)
+    float FireRate;
+    
+    UPROPERTY(Category = Shooting, EditAnywhere, BlueprintReadWrite)
+    FVector GunOffset;
     
     virtual void Tick(float deltaSeconds) override;
+    
+    virtual void FireShot();
+    
+    virtual void BeginPlay() override;
+    
+    FTimerHandle TimerHandle_FireTimerExpired;
+    
+    void FireTimerExpired();
+    
+    
+    
+protected:
+    bool bCanFire;
     
 	
 };
