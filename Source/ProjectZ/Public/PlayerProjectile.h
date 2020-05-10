73a1aa4f8824085a352f7bbb83c5ac82bc0c58fb
dmitrysitnik <dmitry.sitnik46@gmail.com>
2020-05-10 14:38:56 +0300

    starting adding a different projectiles

---
+++ Source/ProjectZ/Public/PlayerProjectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectZProjectile.h"
+#include "PlayerProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZ_API APlayerProjectile : public AProjectZProjectile
+{
+	GENERATED_BODY()
+	
+};
